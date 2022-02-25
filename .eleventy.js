7d8434f17e0398424407b52a17e06d2d0c324671
Dr4g0nsoul <thomas.rizzolli.tr@gmail.com>
2022-02-25 22:58:20 +0100

    setup tailwind

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) { 
+    eleventyConfig.addPassthroughCopy({"src/_includes/assets": "assets"});
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+};
